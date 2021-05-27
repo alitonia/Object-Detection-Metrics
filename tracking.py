61a66d8b8e09912c50059ad9e76f48036acd87e0
Zen-net <allyofjustice1@gmail.com>
2021-05-27 12:47:56 +0700

    new video tracking

---
+++ tracking.py
@@ -0,0 +1,74 @@
+import cv2
+import imutils
+import numpy as np
+import time
+import cv2
+import dlib
+import time
+
+from imutils.video import FileVideoStream
+
+vid_path = 'CHUNG_HA_Bicycle.mp4'
+
+
+def shape_to_np(shape, dtype="int"):
+    # initialize the list of (x, y)-coordinates
+    coords = np.zeros((68, 2), dtype=dtype)
+    # loop over the 68 facial landmarks and convert them
+    # to a 2-tuple of (x, y)-coordinates
+    for i in range(0, 68):
+        coords[i] = (shape.part(i).x, shape.part(i).y)
+    # return the list of (x, y)-coordinates
+    return coords
+
+
+detector = dlib.get_frontal_face_detector()  # replace with detector
+
+face_cascade_default = cv2.CascadeClassifier('other/default_cascade.xml') # remove this
+
+vs = FileVideoStream(vid_path).start()
+time.sleep(2.0)
+
+frame_rate = 30
+prev = time.time()
+
+while vs.more():
+    # grab the frame from the video stream, resize it, and convert it
+    # to grayscale
+    time_elapsed = time.time() - prev
+    if time_elapsed < 1. / frame_rate:
+        continue
+    else:
+        prev = time.time()
+
+    frame = vs.read()
+    frame = imutils.resize(frame, width=450)
+
+    # Replace this with your detect
+    faceRects = []
+    # faceRects = face_cascade_default.detectMultiScale(
+    #     frame, scaleFactor=1.05, minNeighbors=5, minSize=(30, 30),
+    #     flags=cv2.CASCADE_SCALE_IMAGE)
+
+    for (fX, fY, fW, fH) in faceRects:
+        # extract the face ROI
+        faceROI = frame[fY:fY + fH, fX:fX + fW]
+        cv2.rectangle(frame, (fX, fY), (fX + fW, fY + fH),
+                      (0, 255, 0), 2)
+        # apply eyes detection to the face ROI
+        # eyeRects = detectors["eyes"].detectMultiScale(
+        #     faceROI, scaleFactor=1.1, minNeighbors=10,
+        #     minSize=(15, 15), flags=cv2.CASCADE_SCALE_IMAGE)
+        # # apply smile detection to the face ROI
+        # smileRects = detectors["smile"].detectMultiScale(
+        #     faceROI, scaleFactor=1.1, minNeighbors=10,
+        #     minSize=(15, 15), flags=cv2.CASCADE_SCALE_IMAGE)
+
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
+
+cv2.destroyAllWindows()
+vs.stop()
